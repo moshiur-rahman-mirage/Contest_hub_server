aeecacb6dc1366d264d9200e80473489a092b004
moshiur-rahman-mirage <moshiur.mirage@gmail.com>
2023-11-29 02:35:20 +0600

    trying to deploy in vercel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Connect to MongoDB
+
+// Your routes and middleware go here
+
+module.exports = app;
